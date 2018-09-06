932897cbc63cd206996fb82669fd2b46cb10f61b
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-09-06 23:07:33 +0100

    addPhrasetoDisplay()

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const btn__reset = document.querySelector('.btn__reset');
 const qwerty = document.getElementById('qwerty');
 let phraseUL = document.getElementById('phrase');
 let missed = 0; //more than 5 losses the game;
-let ul = phrase.querySelector("ul");
-
+let li 
 
 phrase = ["LUKE CAGE IS GANGSTA NOW","Misty Knight"];
 
@@ -14,27 +13,34 @@ btn__reset.addEventListener('click', function(){
     document.getElementById('overlay').style.display = "none";
 })
 //function
-function getRandomPhraseArray(arr){
+function getRandomPhraseAsArray(arr){
    let randomPhrase =  phrase[Math.floor(Math.random()* arr.length)];
    return randomPhrase.split("");
 } 
-let splitedWords = getRandomPhraseArray(phrase);
-//console.log(splitedWords);
 
-function addPhraseToDisplay(splitedWords){
-    // do stuff any arr that is passed in, and add to `#phrase ul`
+
+function addPhrasetoDisplay(splitedWords){
     for(i = 0; i < splitedWords.length; i++){
-        let li = document.createElement('LI');
-        //If the character in the array is a letter and not a space, the function should add the class “letter” to the list item.
         if(splitedWords[i] != " "){
-            console.log(splitedWords[i]);
-            let li = document.createElement("LI");
-            li.className = "letters";
+            //console.log(splitedWords[i]);
+            li = document.createElement("LI");
+            li.className = "letter";
+            li.textContent = splitedWords[i]
             phraseUL.appendChild(li);
         }else{
            li.className = " ";
         }
     }
+    return li
 }
-console.log(addPhraseToDisplay(splitedWords));
-//console.log(ul);
+const phraseArray = getRandomPhraseAsArray(phrase);
+const words = addPhrasetoDisplay(phraseArray);
+console.log(words);
+
+// function checkLetter(){
+//     for(i = 0; i <= words.length; i++){
+//         console.log(words[i]);
+//     }
+// }
+// checkLetter();
+
