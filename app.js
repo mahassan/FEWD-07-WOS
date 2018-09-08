6a1fd6a21fd7822feff99c8f3de6c2d318226840
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-09-08 21:18:18 +0100

    latest

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const overlay = document;
 const btn__reset = document.querySelector('.btn__reset');
 const qwerty = document.getElementById('qwerty');
-let phraseUL = document.getElementById('phrase');
+let phraseUL = document.querySelector('ul');
 let missed = 0; //more than 5 losses the game;
-let li 
 
 phrase = ["LUKE CAGE IS GANGSTA NOW","Misty Knight"];
 
@@ -17,31 +16,21 @@ function getRandomPhraseAsArray(arr){
    let randomPhrase =  phrase[Math.floor(Math.random()* arr.length)];
    return randomPhrase.split("");
 } 
-
+console.log(getRandomPhraseAsArray(phrase))
 
 function addPhrasetoDisplay(splitedWords){
     for(i = 0; i < splitedWords.length; i++){
-        if(splitedWords[i] === ""){
-            li.className = " ";
+            li = document.createElement("li");
+            li.textContent = splitedWords[i]
+            phraseUL.appendChild(li);
+        if(splitedWords[i] != ''){
+            li.className = "letter";
         }else{
-           //console.log(splitedWords[i]);
-           li = document.createElement("LI");
-           li.className = "letter";
-           li.textContent = splitedWords[i]
-           phraseUL.appendChild(li);
+            li.textContent = " ";
         }
-        return li
     }
+    return li
 }
 const phraseArray = getRandomPhraseAsArray(phrase);
 const words = addPhrasetoDisplay(phraseArray);
-console.log(addPhrasetoDisplay(phraseArray))
-//console.log(words);
-
-// function checkLetter(){
-//     for(i = 0; i <= words.length; i++){
-//         console.log(words[i]);
-//     }
-// }
-// checkLetter();
 
